04aefdda485b87b73f0767efe7e6dee47958b603
Dallon Feldner <dallon@sibipro.com>
2024-12-02 16:36:53 -0600

    Day 2 Part One

--- aoc2024/puzzles/day02.py
+++ aoc2024/puzzles/day02.py
@@ -1,14 +1,43 @@
+import itertools
 import aoc2024.common.input as aoc_input
 
+type Report = list[int]
 
-def parse_reports(lines: list[str]) -> list[list[int]]:
+
+def parse_reports(lines: list[str]) -> list[Report]:
     return [parse_report(line) for line in lines]
 
 
-def parse_report(line: str) -> list[int]:
+def parse_report(line: str) -> Report:
     levels = line.split(" ")
     return [int(x) for x in levels]
 
 
+def is_safe(report: Report) -> bool:
+    direction = None
+    for a, b in itertools.pairwise(report):
+        if a > b:
+            if direction == "increasing":
+                return False
+            direction = "decreasing"
+        elif a < b:
+            if direction == "decreasing":
+                return False
+            direction = "increasing"
+        else:
+            return False  # neither increasing nor decreasing
+
+        diff = abs(a - b)
+        if diff > 3:
+            return False
+    return True
+
+
+def part_one_answer(lines: list[str]) -> int:
+    reports = parse_reports(lines)
+    return sum(1 if is_safe(r) else 0 for r in reports)
+
+
 if __name__ == "__main__":
     puzzle_input = aoc_input.load_lines("day02input")
+    print("Part One:", part_one_answer(puzzle_input))
--- aoc2024/puzzles/day02_test.py
+++ aoc2024/puzzles/day02_test.py
@@ -1,5 +1,5 @@
 import aoc2024.common.input as aoc_input
-from .day02 import parse_reports
+from .day02 import parse_reports, parse_report, is_safe, part_one_answer
 
 SAMPLE_INPUT = aoc_input.load_lines("day02sample")
 
@@ -8,3 +8,19 @@ def test_parse_reports():
     result = parse_reports(SAMPLE_INPUT)
     assert len(result) == 6
     assert result[0:3] == [[7, 6, 4, 2, 1], [1, 2, 7, 8, 9], [9, 7, 6, 2, 1]]
+
+
+def test_is_safe():
+    def is_safe_parsed(line: str) -> bool:
+        return is_safe(parse_report(line))
+
+    assert is_safe_parsed("7 6 4 2 1") == True
+    assert is_safe_parsed("1 2 7 8 9") == False
+    assert is_safe_parsed("9 7 6 2 1") == False
+    assert is_safe_parsed("1 3 2 4 5") == False
+    assert is_safe_parsed("8 6 4 4 1") == False
+    assert is_safe_parsed("1 3 6 7 9") == True
+
+
+def test_part_one_answer():
+    assert part_one_answer(SAMPLE_INPUT) == 2
