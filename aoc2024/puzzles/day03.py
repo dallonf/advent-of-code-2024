5ffdef0276b612ec770fc9e0e7229bf92a198aa2
Dallon Feldner <dallon@sibipro.com>
2024-12-03 10:49:13 -0600

    Day 3 Part Two

--- aoc2024/puzzles/day03.py
+++ aoc2024/puzzles/day03.py
@@ -12,9 +12,39 @@ class MulInstruction:
         return self.a * self.b
 
 
+@dataclass
+class DoInstruction:
+    pass
+
+
+@dataclass
+class DontInstruction:
+    pass
+
+
+type Instruction = MulInstruction | DoInstruction | DontInstruction
+
+
 def extract_mul_instructions(program_memory: str) -> list[MulInstruction]:
     matches = re.findall(r"mul\(([0-9]+),([0-9+]+)\)", program_memory)
     result = [MulInstruction(int(m[0]), int(m[1])) for m in matches]
+    return result
+
+
+def extract_supported_instructions(program_memory: str) -> list[Instruction]:
+    matches = re.findall(r"(do|don't|mul)\(([0-9,]*)\)", program_memory)
+    result: list[Instruction] = []
+    for m in matches:
+        if m[0] == "mul":
+            params = re.match(r"([0-9]+),([0-9+]+)", m[1])
+            if params != None:
+                result.append(
+                    MulInstruction(int(params.group(1)), int(params.group(2)))
+                )
+        elif m[0] == "do" and m[1] == "":
+            result.append(DoInstruction())
+        elif m[0] == "don't" and m[1] == "":
+            result.append(DontInstruction())
     return result
 
 
@@ -23,7 +53,24 @@ def part_one_answer(program_memory: str) -> int:
     return sum(i.execute() for i in instructions)
 
 
+def part_two_answer(program_memory: str) -> int:
+    instructions = extract_supported_instructions(program_memory)
+    result = 0
+    enabled = True
+    for instruction in instructions:
+        match instruction:
+            case MulInstruction():
+                if enabled:
+                    result += instruction.execute()
+            case DoInstruction():
+                enabled = True
+            case DontInstruction():
+                enabled = False
+    return result
+
+
 if __name__ == "__main__":
     puzzle_input = aoc_input.load("day03input")
 
     print("Part One:", part_one_answer(puzzle_input))
+    print("Part Two:", part_two_answer(puzzle_input))
--- aoc2024/puzzles/day03_test.py
+++ aoc2024/puzzles/day03_test.py
@@ -1,4 +1,12 @@
-from .day03 import MulInstruction, extract_mul_instructions, part_one_answer
+from .day03 import (
+    DoInstruction,
+    DontInstruction,
+    MulInstruction,
+    extract_mul_instructions,
+    extract_supported_instructions,
+    part_one_answer,
+    part_two_answer,
+)
 
 SAMPLE_INPUT = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
@@ -15,3 +23,24 @@ def test_extract_mul_instructions():
 
 def test_part_one_answer():
     assert part_one_answer(SAMPLE_INPUT) == 161
+
+
+PART_TWO_SAMPLE_INPUT = (
+    "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+)
+
+
+def test_extract_all_instructions():
+    result = extract_supported_instructions(PART_TWO_SAMPLE_INPUT)
+    print(result)
+    assert result == [
+        MulInstruction(2, 4),
+        DontInstruction(),
+        MulInstruction(5, 5),
+        MulInstruction(11, 8),
+        DoInstruction(),
+        MulInstruction(8, 5),
+    ]
+
+def test_part_two_answer():
+    assert part_two_answer(PART_TWO_SAMPLE_INPUT) == 48
