5d246cda0aa470240729ae4d2a62a18813705562
Dallon Feldner <dallonfdev@gmail.com>
2024-12-04 06:29:23 -0600

    Day 2 Part Two

--- aoc2024/puzzles/day02.py
+++ aoc2024/puzzles/day02.py
@@ -38,6 +38,26 @@ def part_one_answer(lines: list[str]) -> int:
     return sum(1 if is_safe(r) else 0 for r in reports)
 
 
+def is_safe_with_dampener(report: Report) -> bool:
+    initial_result = is_safe(report)
+    if initial_result:
+        return True
+
+    for i in range(len(report)):
+        modified_report = report.copy()
+        modified_report.pop(i)
+        if is_safe(modified_report):
+            return True
+
+    return False
+
+
+def part_two_answer(lines: list[str]) -> int:
+    reports = parse_reports(lines)
+    return sum(1 if is_safe_with_dampener(r) else 0 for r in reports)
+
+
 if __name__ == "__main__":
     puzzle_input = aoc_input.load_lines("day02input")
     print("Part One:", part_one_answer(puzzle_input))
+    print("Part Two:", part_two_answer(puzzle_input))
--- aoc2024/puzzles/day02_test.py
+++ aoc2024/puzzles/day02_test.py
@@ -1,5 +1,12 @@
 import aoc2024.common.input as aoc_input
-from .day02 import parse_reports, parse_report, is_safe, part_one_answer
+from .day02 import (
+    parse_reports,
+    parse_report,
+    is_safe,
+    part_one_answer,
+    is_safe_with_dampener,
+    part_two_answer,
+)
 
 SAMPLE_INPUT = aoc_input.load_lines("day02sample")
 
@@ -24,3 +31,19 @@ def test_is_safe():
 
 def test_part_one_answer():
     assert part_one_answer(SAMPLE_INPUT) == 2
+
+
+def test_is_safe_with_dampener():
+    def is_safe_parsed(line: str) -> bool:
+        return is_safe_with_dampener(parse_report(line))
+
+    assert is_safe_parsed("7 6 4 2 1") == True
+    assert is_safe_parsed("1 2 7 8 9") == False
+    assert is_safe_parsed("9 7 6 2 1") == False
+    assert is_safe_parsed("1 3 2 4 5") == True
+    assert is_safe_parsed("8 6 4 4 1") == True
+    assert is_safe_parsed("1 3 6 7 9") == True
+
+
+def test_part_two_answer():
+    assert part_two_answer(SAMPLE_INPUT) == 4
