e7bb93f20462937c79cad4db86ce7f9ac56fc23e
Dallon Feldner <dallonfdev@gmail.com>
2024-12-11 09:22:09 -0600

    Day 11 Part Two

--- aoc2024/puzzles/day11.py
+++ aoc2024/puzzles/day11.py
@@ -1,43 +1,56 @@
+from functools import cache
 import itertools
-from typing import Callable, Iterable
+from typing import Callable, Iterable, Sequence
 import aoc2024.common.input as aoc_input
 
 
 def parse(line: str):
-    return [int(s) for s in line.split(" ")]
+    return tuple(int(s) for s in line.split(" "))
 
 
-def blink(stones: list[int]) -> list[int]:
-    def stone_reaction(label: int) -> list[int]:
-        if label == 0:
-            return [1]
+def stone_reaction(label: int) -> tuple[int, ...]:
+    if label == 0:
+        return (1,)
 
-        as_str = str(label)
-        if len(as_str) % 2 == 0:
-            midpoint = len(as_str) // 2
-            return [int(as_str[0:midpoint]), int(as_str[midpoint:])]
+    as_str = str(label)
+    if len(as_str) % 2 == 0:
+        midpoint = len(as_str) // 2
+        return (int(as_str[0:midpoint]), int(as_str[midpoint:]))
 
-        return [label * 2024]
-
-    return list(flatmap(stone_reaction, stones))
+    return (label * 2024,)
 
 
-def flatmap[T](func: Callable[[T], list[T]], *iterable: Iterable[T]):
+def blink(stones: tuple[int, ...]) -> tuple[int, ...]:
+    return tuple(flatmap(stone_reaction, stones))
+
+
+def flatmap[T](func: Callable[[T], Sequence[T]], *iterable: Iterable[T]):
     return itertools.chain.from_iterable(map(func, *iterable))
 
 
-def multiblink(stones: list[int], times: int) -> list[int]:
-    for _ in range(times):
-        stones = blink(stones)
-    return stones
+def count_after_blinks(stones: tuple[int, ...], times: int) -> int:
+    return sum(count_single_stone_after_blinks(s, times) for s in stones)
+
+
+@cache
+def count_single_stone_after_blinks(label: int, times: int) -> int:
+    if times == 0:
+        return 1
+    split = stone_reaction(label)
+    return sum(count_single_stone_after_blinks(s, times - 1) for s in split)
 
 
 def part_one_answer(puzzle_input: str) -> int:
     stones = parse(puzzle_input)
-    stones = multiblink(stones, 25)
-    return len(stones)
+    return count_after_blinks(stones, 25)
+
+
+def part_two_answer(puzzle_input: str) -> int:
+    stones = parse(puzzle_input)
+    return count_after_blinks(stones, 75)
 
 
 if __name__ == "__main__":
     puzzle_input = aoc_input.load("day11input")
     print("Part One:", part_one_answer(puzzle_input))
+    print("Part Two:", part_two_answer(puzzle_input))
--- aoc2024/puzzles/day11_test.py
+++ aoc2024/puzzles/day11_test.py
@@ -1,12 +1,12 @@
-from .day11 import blink, multiblink
+from .day11 import blink, count_after_blinks
 
 
 def test_blink():
-    initial = [0, 1, 10, 99, 999]
-    assert blink(initial) == [1, 2024, 1, 0, 9, 9, 2021976]
+    initial = (0, 1, 10, 99, 999)
+    assert blink(initial) == (1, 2024, 1, 0, 9, 9, 2021976)
 
 
-def test_multiblink():
-    initial = [125, 17]
-    assert len(multiblink(initial, 6)) == 22
-    assert len(multiblink(initial, 25)) == 55312
+def test_count_after_blinks():
+    initial = (125, 17)
+    assert count_after_blinks(initial, 6) == 22
+    assert count_after_blinks(initial, 25) == 55312
