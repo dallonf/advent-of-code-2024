5f42d85110e01931b66c2e70a1a831ed169555e5
Dallon Feldner <dallonfdev@gmail.com>
2024-12-16 17:55:09 -0600

    Day 16 Part Two

--- aoc2024/puzzles/day16.py
+++ aoc2024/puzzles/day16.py
@@ -1,3 +1,4 @@
+from collections import deque
 from dataclasses import dataclass
 from typing import Optional, cast
 from aoc2024.common.grid import BasicGrid, Direction, IntVector2
@@ -39,17 +40,28 @@ class Maze:
         assert start is not None and end is not None
         return Maze(walls, start, end)
 
-    def get_path_length(self) -> int:
+    @dataclass(frozen=True)
+    class PathResults:
+        score: int
+        visited_positions: set[IntVector2]
+
+    def explore_path(self) -> "PathResults":
         start: PositionAndDirection = (self.start, Direction.RIGHT)
         frontier_by_priority: list[tuple[PositionAndDirection, int]] = [(start, 0)]
         cost_so_far = dict[PositionAndDirection, int]()
         cost_so_far[start] = 0
+        came_from = dict[PositionAndDirection, set[PositionAndDirection]]()
+        best_score = None
 
         while len(frontier_by_priority) > 0:
             current, _ = frontier_by_priority.pop()
 
             if current[0] == self.end:
-                return cost_so_far[current]
+                if best_score is None:
+                    best_score = cost_so_far[current]
+                if best_score > cost_so_far[current]:
+                    # we're now finding inferior paths
+                    break
 
             clockwise = current[1].clockwise()
             counter_clockwise = current[1].counter_clockwise()
@@ -64,7 +76,7 @@ class Maze:
             ]
             for next, next_cost in neighbors_and_costs:
                 new_cost = cost_so_far[current] + next_cost
-                if next not in cost_so_far or new_cost < cost_so_far[next]:
+                if next not in cost_so_far or new_cost <= cost_so_far[next]:
                     cost_so_far[next] = new_cost
                     priority = new_cost + next[0].manhattan_distance(self.end)
                     bisect.insort(
@@ -74,15 +86,45 @@ class Maze:
                         # is last, to be picked up by pop()
                         key=lambda x: -x[1],
                     )
+                    existing_came_from = came_from.setdefault(next, set())
+                    if next in cost_so_far and new_cost < cost_so_far[next]:
+                        existing_came_from.clear()
+                    existing_came_from.add(current)
 
-        raise AssertionError("no path found")
+        if best_score is None:
+            raise AssertionError("no path found")
+
+        visited_positions = set[IntVector2]()
+        backtrack_queue = deque[PositionAndDirection]((self.end, d) for d in Direction)
+        while len(backtrack_queue) > 0:
+            current = backtrack_queue.popleft()
+            if current in cost_so_far and cost_so_far[current] > best_score:
+                continue
+            visited_positions.add(current[0])
+            prev_nodes = came_from.get(current) or set()
+            backtrack_queue.extend(prev_nodes)
+        return Maze.PathResults(best_score, visited_positions)
+
+    def get_path_score(self):
+        results = self.explore_path()
+        return results.score
+
+    def get_best_seats(self):
+        results = self.explore_path()
+        return results.visited_positions
 
 
 def part_one_answer(lines: list[str]):
     maze = Maze.parse(lines)
-    return maze.get_path_length()
+    return maze.get_path_score()
+
+
+def part_two_answer(lines: list[str]):
+    maze = Maze.parse(lines)
+    return len(maze.get_best_seats())
 
 
 if __name__ == "__main__":
     puzzle_input = aoc_input.load_lines("day16input")
     print("Part One:", part_one_answer(puzzle_input))
+    print("Part Two:", part_two_answer(puzzle_input))
