91ab7f02f0c2823dd12340d8d5a8e9e63be66c0b
Dallon Feldner <dallonfdev@gmail.com>
2024-12-22 22:43:16 -0600

    Day 22 Part Two

--- aoc2024/puzzles/day22.py
+++ aoc2024/puzzles/day22.py
@@ -36,6 +36,50 @@ def part_one_answer(lines: list[str]) -> int:
     return sum(get_nth_secret(s, 2000) for s in secrets)
 
 
+def get_sequences(
+    initial_secret: int, iterations: int = 2000
+) -> dict[tuple[int, int, int, int], int]:
+    result = dict[tuple[int, int, int, int], int]()
+    prices = list[int]()
+    changes = list[int]()
+    secret = initial_secret
+    last_price = initial_secret % 10
+    for _ in range(iterations):
+        new_secret = next_secret(secret)
+        new_price = new_secret % 10
+        prices.append(new_price)
+        changes.append(new_price - last_price)
+        last_price = new_price
+        secret = new_secret
+    assert len(prices) == len(changes)
+    for i in range(3, len(changes)):
+        sequence = (changes[i - 3], changes[i - 2], changes[i - 1], changes[i])
+        price = prices[i]
+        if sequence not in result:
+            result[sequence] = price
+    return result
+
+
+def optimize_purchases(
+    initial_secrets: list[int],
+    iterations: int = 2000,
+) -> tuple[tuple[int, int, int, int], int]:
+    candidates = dict[tuple[int, int, int, int], int]()
+    for secret in initial_secrets:
+        sequences = get_sequences(secret, iterations)
+        for sequence, price in sequences.items():
+            candidates.setdefault(sequence, 0)
+            candidates[sequence] += price
+    return max(candidates.items(), key=lambda it: it[1])
+
+
+def part_two_answer(lines: list[str]) -> int:
+    secrets = map(int, lines)
+    result = optimize_purchases(list(secrets))
+    return result[1]
+
+
 if __name__ == "__main__":
     puzzle_input = aoc_input.load_lines("day22input")
     print("Part One:", part_one_answer(puzzle_input))
+    print("Part Two:", part_two_answer(puzzle_input))
--- aoc2024/puzzles/day22_test.py
+++ aoc2024/puzzles/day22_test.py
@@ -1,4 +1,11 @@
-from .day22 import mix, prune, next_secret, part_one_answer
+from .day22 import (
+    get_sequences,
+    mix,
+    optimize_purchases,
+    prune,
+    next_secret,
+    part_one_answer,
+)
 
 
 def test_mix():
@@ -22,3 +29,18 @@ def test_part_one_answer():
         "2024",
     ]
     assert part_one_answer(sample_input) == 37327623
+
+
+def test_get_sequences():
+    result = get_sequences(123, iterations=10)
+    assert result[(-1, -1, 0, 2)] == 6
+
+
+def test_optimize_purchases():
+    result = optimize_purchases([123], iterations=10)
+    assert result == ((-1, -1, 0, 2), 6)
+
+
+def test_optimize_purchases_large():
+    result = optimize_purchases([1, 2, 3, 2024])
+    assert result == ((-2, 1, -1, 3), 23)
